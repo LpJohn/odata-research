1c2a94ac112fd53ab9ca0667a61763d366042758
johnhilts <jhilts@lampsplus.com>
2016-01-08 22:05:26 -0800

    show data

--- java/odataclient/src/main/java/com/lampsplus/odata_research/client/odataclient/App.java
+++ java/odataclient/src/main/java/com/lampsplus/odata_research/client/odataclient/App.java
@@ -8,10 +8,15 @@ import java.util.Map;
 import org.apache.olingo.client.api.communication.request.retrieve.EdmMetadataRequest;
 import org.apache.olingo.client.api.communication.request.retrieve.ODataServiceDocumentRequest;
 import org.apache.olingo.client.api.communication.response.ODataRetrieveResponse;
+import org.apache.olingo.client.api.domain.ODataEntitySetIterator;
 // import org.apache.olingo.client.api.edm.xml.v4.annotation.Collection;
 import org.apache.olingo.client.api.v4.ODataClient;
 import org.apache.olingo.client.core.ODataClientFactory;
 import org.apache.olingo.commons.api.domain.ODataServiceDocument;
+import org.apache.olingo.commons.api.domain.v4.ODataEntity;
+import org.apache.olingo.commons.api.domain.v4.ODataEntitySet;
+import org.apache.olingo.commons.api.domain.v4.ODataProperty;
+import org.apache.olingo.commons.api.domain.v4.ODataValue;
 import org.apache.olingo.commons.api.edm.Edm;
 import org.apache.olingo.commons.api.edm.EdmComplexType;
 import org.apache.olingo.commons.api.edm.EdmEntityType;
@@ -25,14 +30,20 @@ import org.apache.olingo.commons.api.edm.FullQualifiedName;
  */
 public class App 
 {
+	private final static String _serviceRoot = "http://localhost:62390/odata";
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello OData!" );
+        
+        showMetaData();
+        showData();
+    }
+    
+    private static void showMetaData() {
 
         ODataClient client = ODataClientFactory.getV4();
 
-        String serviceRoot = "http://localhost:62390/odata";
-        ODataServiceDocumentRequest req = client.getRetrieveRequestFactory().getServiceDocumentRequest(serviceRoot);
+        ODataServiceDocumentRequest req = client.getRetrieveRequestFactory().getServiceDocumentRequest(_serviceRoot);
         ODataRetrieveResponse<ODataServiceDocument> res = req.execute();
 
         ODataServiceDocument serviceDocument = res.getBody();
@@ -43,7 +54,7 @@ public class App
         Map<String,URI> functionImports = serviceDocument.getFunctionImports();
         URI productsUri = serviceDocument.getEntitySetURI("Products");
         
-        EdmMetadataRequest request = client.getRetrieveRequestFactory().getMetadataRequest(serviceRoot);
+        EdmMetadataRequest request = client.getRetrieveRequestFactory().getMetadataRequest(_serviceRoot);
         ODataRetrieveResponse<Edm> response = request.execute();
 
         Edm edm = response.getBody();
@@ -70,4 +81,26 @@ public class App
             System.out.println(typeName);
         }
     }
+    
+    private static void showData() {
+        ODataClient client = ODataClientFactory.getV4();
+        URI productsUri = client.newURIBuilder(_serviceRoot).appendEntitySetSegment("Products").build();   	
+        
+        ODataRetrieveResponse<ODataEntitySetIterator<ODataEntitySet, ODataEntity>> response = 
+        		client.getRetrieveRequestFactory().getEntitySetIteratorRequest(productsUri).execute();
+
+        ODataEntitySetIterator<ODataEntitySet, ODataEntity> iterator = response.getBody();
+
+        while (iterator.hasNext()) {
+            ODataEntity customer = iterator.next();
+            List<ODataProperty> properties = customer.getProperties();
+            for (ODataProperty property : properties) {
+                String name = property.getName();
+                ODataValue value = property.getValue();
+                String valueType = value.getTypeName();
+                System.out.println("Name: "+name+"\tValue: "+value + "+\tType:" + valueType);
+            }
+        }
+    }
+    
 }
