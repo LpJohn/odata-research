f544914d063d4f36f46fd15b59ed822f106d83c3
johnhilts <jhilts@lampsplus.com>
2016-01-08 18:07:57 -0800

    OData server prototype. Supports , , etc

---
+++ dotnet/ProductService/ProductService/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using ProductService.Models;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+
+namespace ProductService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // OData specific
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: "odata",
+                model: builder.GetEdmModel());
+        }
+    }
+}
---
+++ dotnet/ProductService/ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using ProductService.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData;
+using System.Web.OData.Routing;
+
+namespace ProductService.Controllers
+{
+    public class ProductsController : ODataController
+    {
+        public IQueryable<Product> Products
+        {
+            get
+            {
+                return (new List<Product> 
+                    {
+                        new Product { Id = 1, Name = "Lamp", Category = "Lighting", Price = 100.0m, } ,
+                        new Product { Id = 2, Name = "Bulb", Category = "Lighting", Price = 19.99m, } ,
+                        new Product { Id = 3, Name = "Chair", Category = "Furniture", Price = 50.0m, } ,
+                        new Product { Id = 4, Name = "Table", Category = "Furniture", Price = 75.8m, } ,
+                    }
+                ).AsQueryable();
+            }
+        }
+
+        [EnableQuery]
+        public IQueryable<Product> Get()
+        {
+            return Products;
+        }
+
+        [EnableQuery]
+        public IQueryable<Product> Get([FromODataUri] int key)
+        {
+            var result = Products.Where(m => m.Id == key);
+            return result;
+        }
+
+    }
+}
+
